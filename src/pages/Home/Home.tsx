868e64a78ef728d655af21dc13c3d984cedf28b1
harlanhai <harlanhai@163.com>
2025-05-10 15:10:54 +0800

    feat: 使用graphql请求worker

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,42 @@
 import React, { useEffect, useState } from 'react';
 
 type Info = {
-  id: number;
+  email: number;
   name: string;
  }
 function Home() {
   const [info, setInfo] = useState<Info>();
   useEffect(() => {
     // 基本的 GET 请求
-    fetch('https://tight-block-8c6c.harlanhai7023.workers.dev/')
-      .then((response) => response.json())
-      .then((data) => {
-        setInfo(data.data);
-        console.log(data)
+    // fetch('https://tight-block-8c6c.harlanhai7023.workers.dev/')
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     setInfo(data.data);
+    //     console.log(data)
+    //   })
+    //   .catch((error) => console.error('Error:', error));
+    fetch("https://my-graphql-api.harlanhai7023.workers.dev", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        query: `
+          {
+            hello
+            user(id: "1") {
+              name
+              email
+            }
+          }
+        `
       })
-      .catch((error) => console.error('Error:', error));
+    })
+    .then(response => response.json())
+    .then(data => {
+      setInfo(data.data.user)
+      console.log(data)
+    });
   }, []);
   return (
     <>
